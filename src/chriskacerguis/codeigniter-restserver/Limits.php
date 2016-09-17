77394c8bbf1eaed63c5d6a0d1d6fc4a584b3da02
Chris Kacerguis <chriskacerguis@gmail.com>
2016-09-16 20:25:32 -0500

    updates to the limit class

--- src/chriskacerguis/codeigniter-restserver/Limits.php
+++ src/chriskacerguis/codeigniter-restserver/Limits.php
@@ -2,13 +2,87 @@
 
 namespace Restserver\Limits;
 
+class Limits {
 
-// is_maxed
+    /**
+     * Constructor for the Limits class
+     *
+     * @access public
+     * @author Chris Kacerguis
+     * @param string $config Configuration filename minus the file extension
+     * @return void
+     */
+    public function __construct($config = 'rest')
+    {
 
-// increase
+    }
 
-// decrease
+    /**
+     * Main method for the limits class.
+     *
+     * @access public
+     * @author Chris Kacerguis
+     * @return bool Indicates of the limit has been reached  
+     */
+    public function check()
+    {
+        // If limits is disabled, then just return true
+        if (!$this->config->item('rest_enable_limits'))
+        {
+            return true;
+        }
 
-// reset_counter
+        // If we are limiting via API Key
+        if ($this->config->item('rest_limit_api_key'))
+        {
+            $at_limit = $this->check_api_key();
+        } 
+        
+        // If we are limiting via IP 
+        if ($this->config->item('rest_limit_ip_address'))
+        {
+            $at_limit = $this->check_ip_address();
+        }
 
-// set
+        // Increment the counter
+        $this->increase();
+        return $at_limit;
+    }
+
+    /**
+     * Checks to see if the IP address is at the MAX limit
+     *
+     * @access public
+     * @author Chris Kacerguis
+     * @return bool Indicates of the limit has been reached  
+     */
+    private function check_ip_address()
+    {
+
+    }
+
+    /**
+     * Checks to see if the API Key is at the MAX limit
+     *
+     * @access public
+     * @author Chris Kacerguis
+     * @return bool Indicates of the limit has been reached  
+     */
+    private function check_api_key()
+    {
+
+    }
+
+    /**
+     * Increase the limit counter of the user
+     *
+     * @access public
+     * @author Chris Kacerguis
+     * @return bool 
+     */
+    private function increase()
+    {
+
+    }
+
+}
